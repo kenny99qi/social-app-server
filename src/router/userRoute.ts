9c76c27a0114587174cd2fd910d4e471067424ee
copsgit <github@copsking.com>
2022-11-24 17:57:34 -0500

    fix

--- src/router/userRoute.ts
+++ src/router/userRoute.ts
@@ -20,7 +20,7 @@ userRouter.post('/auth/register', UserController.registerUser)
 // get user id
 userRouter.get('/getInfo', verifyUser, UserController.getCurrentUserInfo)
 
-userRouter.get('/getOne', verifyUser, UserController.getOneUser)
+userRouter.get('/getOne/:id', verifyUser, UserController.getOneUser)
 
 // logout user if the user login with oAuth
 userRouter.delete('/auth/logout', verifyUser, UserController.logoutUser)
